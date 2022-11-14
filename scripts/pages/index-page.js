5f9ec0982fd1ccad891309b076318aee1473a971
Bryan <mgbryan90@hotmail.com>
2022-11-13 22:31:02 -0500

    Update ShowProducts

--- scripts/pages/index-page.js
+++ scripts/pages/index-page.js
@@ -38,6 +38,15 @@ function showProducts(arregloProducts) {
     cardProduct.appendChild(infoProduct);
     contenedorProducts.appendChild(cardProduct);
   });
+  // Condicional para cuando en el filtrado no se encuentre un producto muestra "Producto no encontrado..."
+  if (contenedorProducts.innerHTML === "") {
+    contenedorProducts.innerHTML += `
+    <div class="productNofinded">
+      <p>Producto no Encontrado...</p>
+      <img src="./images/productoNoEncontrado.png" alt="noEncontrado" class = "noEncontrado"/>
+    </div>
+    `;
+  }
 }
 
 async function getCategories() {
@@ -69,15 +78,6 @@ async function eventfilterProductsByQuery() {
     const queryInput = productFinded.value;
     const products = await getFilterProductsByQuery(queryInput);
     showProducts(products);
-    // Condicional para cuando en el filtrado no se encuentre un producto muestra "Producto no encontrado..."
-    if (contenedorProducts.innerHTML === "") {
-      contenedorProducts.innerHTML += `
-      <div class="productNofinded">
-        <p>Producto no Encontrado...</p>
-        <img src="./images/productoNoEncontrado.png" alt="noEncontrado" class = "noEncontrado"/>
-      </div>
-      `;
-    }
   };
 
   // Evento para el button, cuando se precione se haga el filtro
